f259b10d0d858ec1779ebeb3c8c123b2e5b85f27
Amirhossein Alibakhshi <amir78729@gmail.com>
2025-12-19 18:27:34 +0330

    Update version and SHA256 for landing prompt generator

--- landing-prompt-gen.rb
+++ landing-prompt-gen.rb
@@ -2,8 +2,8 @@ class LandingPromptGen < Formula
   desc "Generate filled prompts from templates by fetching API data"
   homepage "https://github.com/amir78729/figma-make-landing-prompt-generator"
   url "https://github.com/amir78729/figma-make-landing-prompt-generator/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "c98b6903961dd858495f50a41120d2e14bc7e1e93b2df37bd653ed5b7043c743"
-  version "0.0.1"
+  sha256 "88c2f6f7bbb134386034516757215e349466a63884433a03b4184d01bb576cc5"
+  version "0.0.2"
 
   def install
     bin.install "generate-prompt.sh" => "landing-prompt-gen"
